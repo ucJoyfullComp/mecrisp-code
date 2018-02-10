8600475a09969cfd5c238813b661043f6da1e0c2
Uzi Cohen <uc.metoo@gmail.com>
2018-02-10 07:18:45 +0200

    Update IncludeAll.fs

--- stm32f103/compiled/IncludeAll.fs
+++ stm32f103/compiled/IncludeAll.fs
@@ -1,12 +1,19 @@
 compiletoflash
 
-include 00.fs
-include 01.fs
-include 02.fs
-include 03.fs
-include 04.fs
-include 05.fs
-include 06.fs
+\ some general words missing in mecrisp-stellaris
+include 00_xtrafunc.fs
+\ general STM32F103 Base addresses and register mappings for single function blocks
+include 01_regdefs.fs
+\ STM32F103 register offsets for multiple instanced functions
+include 02_regdefsofs.fs
+\ driver for controlling the system clocks and PLL
+include 03_sysclkpll.fs
+\ driver and words for setting the SYSTICK block of the Cortex-M3 (1 mili-second)
+include 04_systick.fs
+\ generate frequency from TIM4 used also to drive the ADC driver sampling
+include 05_genfreqtim4.fs
+\ driver for ADC1 and ADC2 in multi-channel mode using the DMA and TIM4 as clock driver
+include 06_adcmcdrvr.fs
 
 compiletoram
 
